a803a52b869dc54b581cd24813b9bd41d3101bfc
sakshi <skumaribth9300@gmail.com>
2026-07-31 18:30:26 +0530

    Create string.py

---
+++ Basics/string.py
@@ -0,0 +1,25 @@
+name = "Sakshi Kumari"
+print(name)
+
+# Length of string
+print("Length:", len(name))
+
+# Uppercase
+print("Upper:", name.upper())
+
+# Lowercase
+print("Lower:", name.lower())
+
+# Replace
+print(name.replace("Sakshi", "Miss"))
+
+# Slice
+print(name[:6])
+
+#Removing whitespace
+print(a.strip())
+
+# Formate string
+age = 19
+txt = f"My name is {name}, I am {age}"
+print(txt)
